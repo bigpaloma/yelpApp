e6877011ce4339d1c2943621aefaf66403650c2f
bigpaloma <senguenfurkan@gmail.com>
2023-09-02 13:14:06 +0200

    started refactoring to passport

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const ejsMate = require("ejs-mate");
 const Joi = require("joi");
 const session = require("express-session");
 const flash = require("express-flash");
+const passport = require("passport");
+const passportLocal = require("passport-local");
 
 const AppError = require("./errors/appError");
+
+const User = require("./models/user");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
@@ -55,6 +59,13 @@ app.use((req, res, next) => {
   res.locals.error = req.flash("error");
   next();
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 const bcrypt = require("bcrypt");
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelpApp")
@@ -12,36 +13,48 @@ mongoose
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  username: {
+//AUTH WITH PASSPORT
+const UserSchema = new Schema({
+  email: {
     type: String,
-    required: [true, "username is required"],
-  },
-  firstname: {
-    type: String,
-    required: [true, "firstname is required"],
-  },
-  name: {
-    type: String,
-    required: [true, "name is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
+    required: true,
+    unique: true,
   },
 });
 
-userSchema.statics.findAndValidate = async function (username, password) {
-  const user = await this.findOne({ username });
-  const validPassword = await bcrypt.compare(password, user.password);
-  return validPassword ? user : false;
-};
+UserSchema.plugin(passportLocalMongoose);
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  //   this.password = "HIJACKED!";
-  next();
-});
+// AUTH WITHOUT PASSPORT
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: [true, "username is required"],
+//   },
+//   firstname: {
+//     type: String,
+//     required: [true, "firstname is required"],
+//   },
+//   name: {
+//     type: String,
+//     required: [true, "name is required"],
+//   },
+//   password: {
+//     type: String,
+//     required: [true, "password is required"],
+//   },
+// });
 
-module.exports = mongoose.model("User", userSchema);
+// userSchema.statics.findAndValidate = async function (username, password) {
+//   const user = await this.findOne({ username });
+//   const validPassword = await bcrypt.compare(password, user.password);
+//   return validPassword ? user : false;
+// };
+
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   this.password = await bcrypt.hash(this.password, 12);
+//   //   this.password = "HIJACKED!";
+//   next();
+// });
+
+module.exports = mongoose.model("User", UserSchema);
